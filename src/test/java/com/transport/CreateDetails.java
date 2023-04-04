033e44a74793d7179403dd6e3b15757adafef061
panth <panth@hp>
2023-04-04 12:20:24 +0530

    this is framework

---
+++ src/test/java/com/transport/CreateDetails.java
@@ -0,0 +1,10 @@
+package com.transport;
+
+public class CreateDetails {
+
+	public static void main(String[] args) {
+		System.out.println("hi ! details are here");
+
+	}
+
+}
---
+++ src/test/java/com/transport/CreateProfile.java
@@ -0,0 +1,10 @@
+package com.transport;
+
+public class CreateProfile {
+
+	public static void main(String[] args) {
+		System.out.println("hy!upload profile please");
+
+	}
+
+}
